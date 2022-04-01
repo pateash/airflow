749f2994d6fc809e0f902860bd8e01fb117a5b68
Ashish Patel <ashishpatel0720@gmail.com>
2022-04-01 23:18:40 +0100

    airflow-10893 - adding initial files

--- setup.py
+++ setup.py
@@ -238,6 +238,12 @@ cgroups = [
     # Cgroupspy 0.2.2 added Python 3.10 compatibility
     'cgroupspy>=0.2.2',
 ]
+clickhouse = [
+    'clickhouse-driver>=0.2.3',
+]
+cloudant = [
+    'cloudant>=2.0',
+]
 dask = [
     # Dask support is limited, we need Dask team to upgrade support for dask if we were to continue
     # Supporting it in the future
@@ -566,6 +572,7 @@ ALL_DB_PROVIDERS = [
     'apache.hive',
     'apache.pinot',
     'arangodb',
+    'clickhouse',
     'cloudant',
     'databricks',
     'exasol',
